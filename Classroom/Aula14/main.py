0bae8418f537a583070fdd6294cad59f68feb241
Ulisses Junior <ulissesjunior2005@gmail.com>
2025-05-12 17:18:27 -0300

    feat: add aula14 notes-2

--- Classroom/Aula14/main.py
+++ Classroom/Aula14/main.py
@@ -1,64 +1,62 @@
 import csv
 
 def parse_percent(value):
-    return float(value.strip('%'))
+  return float(value.strip('%'))
 
 def parse_float(value):
-    return float(value)
+  return float(value)
 
 def carregar_dados_csv(caminho_arquivo):
-    dados = []
-    with open(caminho_arquivo, newline='', encoding='utf-8') as csvfile:
-        leitor = csv.reader(csvfile)
-        cabecalho = next(leitor)
-        #dados.append(cabecalho) se quiser manter o cabeçalho
+  dados = []
+  with open(caminho_arquivo, newline='', encoding='utf-8') as csvfile:
+    leitor = csv.reader(csvfile)
+    next(leitor)
+    
+    for linha in leitor:
+      nome = linha[0]
+      roles = [r.strip() for r in linha[1].split(',')]
+      pickrate = parse_percent(linha[2])
+      winrate = parse_percent(linha[3])
+      banrate = parse_percent(linha[4])
+      kills = parse_float(linha[5])
+      deaths = parse_float(linha[6])
+      assists = parse_float(linha[7])
+      
+      kda = int(round(((kills + assists) / deaths), 3) * 1000)
+      
+      dados.append((kda, len(dados), nome))
+  return dados
 
-        for linha in leitor:
-            nome = linha[0]
-            roles = [r.strip() for r in linha[1].split(',')]  # ['Mid', 'Top'], por exemplo
-            pickrate = parse_percent(linha[2])
-            winrate = parse_percent(linha[3])
-            banrate = parse_percent(linha[4])
-            kills = parse_float(linha[5])
-            deaths = parse_float(linha[6])
-            assists = parse_float(linha[7])
-            pentakills = parse_float(linha[8])
-
-            dados.append([nome, roles, pickrate, winrate, banrate, kills, deaths, assists, pentakills])
-    return dados
-
-def CountingSortDigito(A, d):
-  B = [None for _ in range(0, len(A))]
-  C = [0 for _ in range(0, 10)]
+def counting_sort_digito(A, d):
+  B = [None] * len(A)
+  C = [0] * 10
+  
   for a in A:
-    indice = (a // (10**d)) % 10
+    indice = (a[0] // (10**d)) % 10
     C[indice] += 1
+    
   for i in range(1, 10):
-    C[i] = C[i] + C[i-1]
+    C[i] += C[i-1]
+    
   for i in range(len(A)-1, -1, -1):
     a = A[i]
-    indice = (a // (10**d)) % 10
-    B[C[indice] - 1] = a 
-    C[indice] -= 1 
+    indice = (a[0] // (10**d)) % 10
+    B[C[indice] - 1] = a
+    C[indice] -= 1
+    
   return B
 
-
-def RadixSort(A, D):
-  for d in range(0, D):
-    A = CountingSortDigito(A, d)
-    A = list(reversed(A))
-        
+def radix_sort(A, D):
+  for d in range(D):
+    A = counting_sort_digito(A, d)
   return A
 
-# Exemplo de uso
 if __name__ == "__main__":
-    dados = carregar_dados_csv('champs.csv')
-    kdas = []
-    champions = {}
-    for linha in dados:
-        kda = int(round(((linha[5] + linha[7]) / linha[6]), 3) * 1000)
-        # kdas.append([linha[0], kda])
-        kdas.append(kda)
-        champions[linha[0] + str(kda)] = kda
-        
-    print(RadixSort(kdas, 4))   
+  dados = carregar_dados_csv('champs.csv')
+
+  dados_ordenados = radix_sort(dados, 4)
+  
+  campeoes_ordenados = [nome for (kda, pos, nome) in reversed(dados_ordenados)]
+  
+  for campeao in campeoes_ordenados:
+    print(campeao)
